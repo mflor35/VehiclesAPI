6de55c8908ca0e3efe2b8bed087f84e04cea7490
Miguel Flores Silverio <floresmigu3l@gmail.com>
2019-10-06 14:37:54 -0700

    Add simple integration test

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -2,15 +2,30 @@ package com.udacity.pricing;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
 
+	@Autowired
+    private MockMvc mvc;
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void getPrice() throws Exception {
+		RequestBuilder request = get("/services/price").param("vehicleId","1");
+        mvc.perform(request).andExpect(status().isOk());
+	}
 }
