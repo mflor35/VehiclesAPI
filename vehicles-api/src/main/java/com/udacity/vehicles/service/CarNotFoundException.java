4f08528530f871da58d5adc06f3ad6c161eab3b1
Miguel Flores Silverio <floresmigu3l@gmail.com>
2019-10-06 03:33:04 -0700

    misc clean up

--- vehicles-api/src/main/java/com/udacity/vehicles/service/CarNotFoundException.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/service/CarNotFoundException.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Car not found")
 public class CarNotFoundException extends RuntimeException {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
     public CarNotFoundException() {
     }
 
